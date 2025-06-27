f0c3743f56dc73b72d70d83bde07ce15df9f00a3
saraiva142 <saraivajandrade@gmail.com>
2025-06-27 14:05:47 -0300

    Já criado o entity, repository e service de Livro

---
+++ src/main/java/apiTeste/livraria/entitie/Livro.java
@@ -0,0 +1,22 @@
+package apiTeste.livraria.entitie;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "tb_livro")
+public class Livro implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    //private Autor autor;
+}
---
+++ src/main/java/apiTeste/livraria/repository/LivroRepository.java
@@ -0,0 +1,7 @@
+package apiTeste.livraria.repository;
+
+import apiTeste.livraria.entitie.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+}
