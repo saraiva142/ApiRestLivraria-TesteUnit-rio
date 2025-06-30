162af3d09ad2d61938966fcb5d6151f09785d46c
saraiva142 <saraivajandrade@gmail.com>
2025-06-30 13:31:25 -0300

    Começando Testes - Service 🦥

---
+++ src/test/java/apiTeste/livraria/service/AutorServiceTest.java
@@ -0,0 +1,27 @@
+package apiTeste.livraria.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class AutorServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void update() {
+    }
+}
