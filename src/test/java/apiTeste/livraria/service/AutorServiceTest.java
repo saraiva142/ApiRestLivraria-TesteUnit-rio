4dfcc190ab4391e880896600c89070a6c9a86536
saraiva142 <saraivajandrade@gmail.com>
2025-06-30 14:21:01 -0300

    Teste de Create Autor OK 🐢

--- src/test/java/apiTeste/livraria/service/AutorServiceTest.java
+++ src/test/java/apiTeste/livraria/service/AutorServiceTest.java
@@ -1,16 +1,74 @@
 package apiTeste.livraria.service;
 
+import apiTeste.livraria.entity.Autor;
+import apiTeste.livraria.repository.AutorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
 class AutorServiceTest {
 
-    @Test
-    void create() {
+    // Uma das dependências lá no service é o AutorRepository, então temos que mockar ele
+
+    @Mock
+    private AutorRepository autorRepository;
+
+    @Autowired
+    @InjectMocks
+    private AutorService autorService;
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.initMocks(this);
     }
 
-    @Test
-    void delete() {
+
+    @Nested
+    class createAutor {
+
+        @Test
+        @DisplayName("Should create autor with success")
+        void createAutorWihtSuccess() {
+            //Arrange
+            Autor autorToSave = new Autor();
+            autorToSave.setNome("Michael Sullivan");
+
+            Autor savedAutor = new Autor();
+            savedAutor.setId(1L);
+            savedAutor.setNome("Michael Sullivan");
+
+            Mockito.when(autorRepository.save(autorToSave)).thenReturn(savedAutor);
+
+            //Act
+            Autor result = autorService.create(autorToSave);
+
+            //Assert
+            assertNotNull(result);
+            assertEquals(1L, result.getId());
+            assertEquals("Michael Sullivan", result.getNome());
+            Mockito.verify(autorRepository, Mockito.times(1)).save(autorToSave);
+        }
+    }
+
+    @Nested
+    class deleteAutor {
+        @Test
+        @DisplayName("Shoul delete by id with success")
+        void deleteAutorByIdWithSuccess() {
+
+        }
+
     }
 
     @Test
