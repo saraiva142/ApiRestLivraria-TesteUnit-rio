ef47b8e53d82f0fb3a7f951b1ca883eca8f7028a
saraiva142 <saraivajandrade@gmail.com>
2025-07-01 13:14:26 -0300

    Começando os testes de Livro Service 📖

---
+++ src/test/java/apiTeste/livraria/service/LivroServiceTest.java
@@ -0,0 +1,28 @@
+package apiTeste.livraria.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LivroServiceTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void update() {
+    }
+}
