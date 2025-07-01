6301c3456023b9520b985299b5d4806e8f3532cc
saraiva142 <saraivajandrade@gmail.com>
2025-07-01 14:00:10 -0300

    Teste de crete livro Ok 📖

--- src/test/java/apiTeste/livraria/service/LivroServiceTest.java
+++ src/test/java/apiTeste/livraria/service/LivroServiceTest.java
@@ -1,14 +1,62 @@
 package apiTeste.livraria.service;
 
+import apiTeste.livraria.entity.Livro;
+import apiTeste.livraria.repository.LivroRepository;
+import apiTeste.livraria.service.exception.EntityNotFound;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class LivroServiceTest {
 
-    @Test
-    void create() {
+    @Mock
+    private LivroRepository livroRepository;
+
+    @Autowired
+    @InjectMocks
+    private LivroService livroService;
+
+    @Nested
+    class createLivro{
+        @Test
+        @DisplayName("Should create livro successfully")
+        void createLivroWithSuccess() {
+            //Arrange
+            Livro newLivro = new Livro();
+            newLivro.setNome("Java Livro");
+
+            Livro livroSaved = new Livro();
+            livroSaved.setId(1L);
+            livroSaved.setNome("Java Livro");
+
+            when(livroRepository.save(newLivro)).thenReturn(livroSaved);
+
+            //Act
+            Livro result = livroService.create(newLivro);
+
+            //Assert
+            assertNotNull(result);
+            assertEquals(1l, result.getId());
+            assertEquals(livroSaved.getNome(), result.getNome());
+            verify(livroRepository, times(1)).save(newLivro);
+        }
     }
+
+
 
     @Test
     void delete() {
