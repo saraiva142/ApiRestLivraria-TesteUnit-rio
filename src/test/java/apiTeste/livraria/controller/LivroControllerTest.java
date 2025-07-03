65cab28bdc5fe467b5a60cbccbc7c0afc325d4bc
saraiva142 <saraivajandrade@gmail.com>
2025-07-03 14:03:35 -0300

    Começando os testes para o Livro Controller 📒

---
+++ src/test/java/apiTeste/livraria/controller/LivroControllerTest.java
@@ -0,0 +1,28 @@
+package apiTeste.livraria.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LivroControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void update() {
+    }
+}
